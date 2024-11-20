20c17a59291767dccd44a3e988b872bdbc5f88b8
raza-ahmed-khan360 <razaahmedkhan360@gmail.com>
2024-11-20 09:27:44 +0500

    TodoList.tsx

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -11,8 +11,8 @@ type TodoListProps = {
 export default function TodoList({ todos, editItem, delItem }: TodoListProps) {
   return (
     <div>
-      <h2 className="text-center font-bold text-blue-900 text-[40px] my-4">Task List</h2>
-    <div className="grid sm:grid-cols-2 mt-5 gap-5 grid-cols-1 ">
+      <h2 className="text-center font-bold text-blue-900 sm:text-[32px] text-[28px] md:text-[38px] lg:text-[40px] my-4">Task List</h2>
+    <div className="grid sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 mt-5 gap-5 grid-cols-1 ">
       {todos.map((item) => (
         <TodoItem key={item.id} todo={item} editItem={editItem} delItem={delItem} />
       ))}
